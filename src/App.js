a233ceead1e664c23fdbe27fdc44ad46f67391d6
Siow Wen Ying <hanasiow@hotmail.com>
2020-10-29 16:32:42 +0800

    developed first page with link button and style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import './App.css';
-import firebase from '../Config';
+import firebase from './Config';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {Card} from 'react-bootstrap';
+import {Link} from 'react-router-dom'
 
 class App extends React.Component{
   constructor(props){
     super(props);
   }
   render(){
+    const cardStyle ={
+      width: 'auto',
+      height: 'auto',
+      backgroundColor: 'white',
+      margin: 'auto',
+      display: 'block',
+      marginTop: '60px',
+      opacity: 0.5,
+      paddingTop: '10px',
+      paddingLeft: '20px',
+      paddingRight: '20px',
+      borderStyle: 'outset',
+      borderLeft: '50px solid black',
+      borderRadius: '20px'
+    }
     return(
       <div>
+        <Card style = {cardStyle}>
+          <div className = "Buttons">
+            <Link to = "/create">
+              <button class= "Add-Button">Add Product</button>
+            </Link>
+          </div>
 
+        </Card>
       </div>
     )
   }
--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,8 @@
 import* as firebase from 'firebase';
 
-const settings = {timestampsInSnapshot: true}
+const settings = {timestampsInSnapshots: true}
 
-var firebaseConfig ={
+var firebaseConfig = {
     apiKey: "AIzaSyCu5aqMDEIAXhQ11OzwmgSq7vD1VeAFs28",
     authDomain: "reactproject-78cea.firebaseapp.com",
     databaseURL: "https://reactproject-78cea.firebaseio.com",
@@ -10,9 +10,8 @@ var firebaseConfig ={
     storageBucket: "reactproject-78cea.appspot.com",
     messagingSenderId: "1089193116453",
     appId: "1:1089193116453:web:ac421e5376110f9fa4d0cc"
-};
-// Initialize Firebase
+  };
   firebase.initializeApp(firebaseConfig);
-  firebase.firestone().settings(settings);
+  firebase.firestore().settings(settings);
 
   export default firebase;
